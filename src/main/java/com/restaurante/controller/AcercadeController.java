67475210560cf9216d95a2b715c21bead920bfda
edra0 <edra0@ED>
2024-07-18 19:53:38 -0600

    No se logro el Acerca de

---
+++ src/main/java/com/restaurante/controller/AcercadeController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.restaurante.controller;
+
+import com.restaurante.controller.*;
+import com.restaurante.domain.Categoria;
+import com.restaurante.domain.Producto;
+import com.restaurante.services.CategoriaService;
+import com.restaurante.services.ProductoService;
+import com.restaurante.services.FirebaseStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import com.restaurante.services.CategoriaService;
+
+/**
+ *
+ * @author Ema
+ */
+@Controller
+@RequestMapping("/acercade")
+public class AcercadeController {
+
+   
+    }
+
